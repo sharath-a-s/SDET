b4a52918f9bcbbffaaefd3818e643f1d754af501
AdderiS <AdderiS@BGLRT4800380.bsg.ad.adp.com>
2021-06-14 16:24:19 +0530

    Factorial Progaram update

--- SDET/src/factorialProgram.java
+++ SDET/src/factorialProgram.java
@@ -1,8 +1,25 @@
+import java.util.Scanner;
 
 public class factorialProgram {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		int inputNumber;
+		System.out.println("Enter the number: ");
+		
+		Scanner scanner= new Scanner(System.in);
+		inputNumber= scanner.nextInt();
+		
+		int i=1;
+		int fact=1;
+		while(i<=inputNumber)
+		{
+			fact= fact *i;
+			i++;
+			
+		}
+		System.out.println("factorial of"+" "+inputNumber+" "+"is :"+fact);
 
 	}
 
