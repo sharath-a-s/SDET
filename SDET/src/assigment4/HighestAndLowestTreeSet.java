d989aaba6b56dd40237602937efbd4fd144514b3
AdderiS <AdderiS@BGLRT4800380.bsg.ad.adp.com>
2021-06-21 16:02:05 +0530

    Get highest and lowest value stored in TreeSet

---
+++ SDET/src/assigment4/HighestAndLowestTreeSet.java
@@ -0,0 +1,18 @@
+package assigment4;
+
+import java.util.*;
+public class HighestAndLowestTreeSet {
+	@SuppressWarnings("unchecked")
+	public static void main(String args[]) {
+        @SuppressWarnings("rawtypes")
+		TreeSet output = new TreeSet();
+       
+        output.add("selenium");
+        output.add("Training");
+        output.add("Session");
+       
+        System.out.println("TreeSet elements - " + output);
+        System.out.println("Highest element - " + output.first());
+        System.out.println("Lowest element - " + output.last());
+    }
+}
