9fff45fde3e2a8c9e2ae5db03b88f7c2937763b8
AdderiS <AdderiS@BGLRT4800380.bsg.ad.adp.com>
2021-06-21 15:46:24 +0530

    Reverse Array

---
+++ SDET/src/assigment4/Reverse.java
@@ -0,0 +1,27 @@
+package assigment4;
+
+import java.util.*;
+public class Reverse {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  List<String> output = new ArrayList<String>();
+
+	        output.add("Selenium");
+
+	        output.add("TestNg");
+
+	        output.add("cucumber");
+
+	        
+
+	        System.out.println("Elements before reversing - " + output);
+
+	        
+
+	        Collections.reverse(output);  
+
+	        System.out.println("Elements after reversing - " + output);
+	}
+
+}
