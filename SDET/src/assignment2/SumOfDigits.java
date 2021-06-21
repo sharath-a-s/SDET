213f4c03d7e931939249fc2574e3c8a46c407063
AdderiS <AdderiS@BGLRT4800380.bsg.ad.adp.com>
2021-06-21 13:09:19 +0530

    Sum of digits

---
+++ SDET/src/assignment2/SumOfDigits.java
@@ -0,0 +1,23 @@
+package assignment2;
+
+import java.util.Scanner;
+
+public class SumOfDigits {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);  
+        System.out.print("Enter a number - ");
+        int num = sc.nextInt(); 
+        int sum = 0;
+        while(num != 0) {
+        	sum = sum + num % 10;
+        	num = num / 10;
+        }
+        System.out.print("Sum of digits - "+ sum);
+
+	}
+
+
+}
+
+
