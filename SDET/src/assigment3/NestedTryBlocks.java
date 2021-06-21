eed54c06753526815038c6c23d75b0cd79356755
AdderiS <AdderiS@BGLRT4800380.bsg.ad.adp.com>
2021-06-21 14:35:55 +0530

    Nested Try Block

---
+++ SDET/src/assigment3/NestedTryBlocks.java
@@ -0,0 +1,29 @@
+package assigment3;
+
+public class NestedTryBlocks {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			try {
+				System.out.println("divide");
+				int b = 100 / 0;
+			} catch (ArithmeticException e) {
+				System.out.println(e);
+			}
+
+			try {
+				System.out.println("Array");
+				int a[] = new int[10];
+				a[10] = 8;
+			} catch (ArrayIndexOutOfBoundsException e) {
+				System.out.println(e);
+			}
+
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+
+	}
+
+}
